6ece6ec86a00113dd58eae4426fbbb7843e39f67
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-09-29 22:55:48 +0530

    136

---
+++ 136.Single_Number.cpp
@@ -0,0 +1,11 @@
+// Leetcode Solution
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int a:nums){
+            ans=ans^a;
+        }
+        return ans;
+    }
+};
