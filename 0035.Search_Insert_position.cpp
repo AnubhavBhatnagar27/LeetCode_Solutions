c5c223fb599f82a81d573c81465dedda9202bf7b
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-10-14 20:21:14 +0530

    Day 14

---
+++ 0035.Search_Insert_position.cpp
@@ -0,0 +1,19 @@
+// Leetcode Solution
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int st=0,end=nums.size()-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]<target){
+                st=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return st;
+    }
+};
