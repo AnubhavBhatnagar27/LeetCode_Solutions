9916b8b5da30f609036c52e3f2ec98bc2e31677b
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-10-21 22:10:01 +0530

    Day 21

---
+++ 1593.Split_A_String.cpp
@@ -0,0 +1,27 @@
+// Leetcode Solution
+
+
+class Solution {
+public:
+    int maxUniqueSplit(string s) {
+        unordered_set<string> seen;
+        return dfs(0, s, seen);
+    }
+
+private:
+    int dfs(int start, const string& s, unordered_set<string>& seen) {
+        if (start == s.size()) return 0;
+
+        int maxSplits = 0;
+        for (int i = start + 1; i <= s.size(); ++i) {
+            string substring = s.substr(start, i - start);
+
+            if (seen.find(substring) == seen.end()) {
+                seen.insert(substring);
+                maxSplits = max(maxSplits, 1 + dfs(i, s, seen));
+                seen.erase(substring);  // Backtrack
+            }
+        }
+        return maxSplits;
+    }
+};
