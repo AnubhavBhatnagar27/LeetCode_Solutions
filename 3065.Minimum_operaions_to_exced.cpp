3be0d1e002c805f671baa873760b1bf2ec4de3b7
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-10-25 20:05:58 +0530

    Day 25

---
+++ 3065.Minimum_operaions_to_exced.cpp
@@ -0,0 +1,11 @@
+// Leetcode Solution
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<k)   count++;
+        }
+        return count;
+    }
+};
