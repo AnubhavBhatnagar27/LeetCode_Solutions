008ac6bf710f27f72f685848f97367da71243baf
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-11-08 20:33:49 +0530

    Day 39

---
+++ 0374.Guess_Number_HIgher_or_lower.cpp
@@ -0,0 +1,35 @@
+// Leetcode Solution
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        if(guess(n)==0) return n;
+        int st=1;
+        int end =n;
+        while(st<=end)
+        {
+            int mid=st+(end-st)/2;
+            if(guess(mid)==0){
+                return mid;
+            }
+            else if(guess(mid)==1){
+                st=mid+1;
+                end=n;
+            }
+            else
+            {
+                st=st;
+                end=mid-1;
+            }        
+        }
+        return -1;
+    }
+};
