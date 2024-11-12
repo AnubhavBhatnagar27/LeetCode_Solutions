244d5fc77f203ce00c197158c16c26d7783143db
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-11-12 21:59:25 +0530

    Day 43

--- 0151.Reverse_words_in_a_string.cpp
+++ 0151.Reverse_words_in_a_string.cpp
@@ -24,3 +24,27 @@ public:
         return s;
     }
 };
+
+// Optimised Solution
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.size();
+        string ans="";
+        reverse(s.begin(),s.end());
+        for(int i=0;i<n;i++){
+            if(s[i]== ' ') continue;
+            string word="";
+            while(i<n && s[i]!=' '){
+                word +=s[i];
+                i++;
+            }
+            reverse(word.begin(),word.end());
+            if(!ans.empty()){
+                ans+=" ";
+            }
+            ans+=word;
+        }
+        return ans;   
+    }
+};
