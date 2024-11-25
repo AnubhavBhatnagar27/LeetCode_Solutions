ea9a54c6c7191950515b4ff60a6bf44da4d2e1dd
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-11-25 20:10:37 +0530

    Day 56

--- 0231.Power_of_two.cpp
+++ 0231.Power_of_two.cpp
@@ -6,3 +6,17 @@ public:
         return  (n>0 && (n & (n-1))==0) ? true:false;
     }
 };
+
+// Leetcode Solution
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        for(long int i=1;i<=n;i=2*i){
+            if(i==n){
+                return true;
+            }
+        }
+    return false;
+    }
+};
