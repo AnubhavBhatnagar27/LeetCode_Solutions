98748dce57c167d94adf5f65b702e49712d5f9d2
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-12-07 23:39:07 +0530

    Day 68

---
+++ 1752. CheckifArrayIsSortedandRotated.cpp
@@ -0,0 +1,18 @@
+// Leetcode Solution
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
