34f95db31b5845794f1407b08aa7055206eb677d
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-12-15 11:33:36 +0530

    Day 76

---
+++ 1342.Number_of_steps_to_reduce_a_number_to_zero.cpp
@@ -0,0 +1,13 @@
+// Leetcode Solution
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int count=0;
+        while(num!=0){
+            if(num%2==0) num=num>>1;
+            else num--;
+            count++;
+        }
+        return count;
+    }
+};
